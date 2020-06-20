1836a39c082d8fc1229eb277db47206ee8c5ebc5
Mike Goldberg <mkegoldberg@yahoo.com>
2020-06-20 14:01:31 -0700

    adding 8.2

---
+++ week-8/goldberg_hello_world.py
@@ -0,0 +1,18 @@
+
+# == == == == == == == == == == == == == == == == == == == == == ==
+# Title: Assignment
+# Author: Mike Goldberg
+# Date:  06/20/2020
+# Modified By:
+# Description: Python
+# == == == == == == == == == == == == == == == == == == == == == =
+
+# start program
+
+# expected output:
+# Hello World
+
+
+print('Hello World')
+
+# end program
