3a21cd62c3fe3bf7dcf129589c5b3edc88bf6d1e
Mike Goldberg <mkegoldberg@yahoo.com>
2020-06-26 19:11:59 -0700

    adding 9.3

---
+++ week-9/goldberg-user-update.py
@@ -0,0 +1,39 @@
+
+# == == == == == == == == == == == == == == == == == == == == == ==
+# Title: Assignment 9.3
+# Author: Mike Goldberg
+# Date:  06/26/2020
+# Description: Updating and Deleting Documents
+# == == == == == == == == == == == == == == == == == == == == == =
+
+# start program
+
+# expected output:
+
+# "email": "migoldberg@my365.bellevue.edu"
+# "employee_id": "1234567"
+# "first_name": "John"
+# "last_name" "Test"
+
+
+from pymongo import MongoClient
+
+import pprint
+import datetime
+
+client = MongoClient('localhost', 27017)
+db = client.web335
+
+db.users.update_one(
+    {"employee_id": "1234567"},
+  {
+    "$set": {
+        "email": "migoldberg@my365.bellevue.edu"
+    }
+  }
+)
+
+# output
+pprint.pprint(db.users.find_one({"employee_id": "1234567"}))
+
+# end program
